f1135b3876ad148f3a011e61c6bcceefdd723bd8
Jinwon <jinwon11022@naver.com>
2024-10-01 22:51:35 +0900

    feat: 캐릭터의 스테이지 이동기능 테스트 추가

--- Assets/Scenes/2.StageScene/Scripts/StageMgr.cs
+++ Assets/Scenes/2.StageScene/Scripts/StageMgr.cs
@@ -7,14 +7,33 @@ using UnityEngine;
 public class StageMgr : MonoBehaviour
 {
     public Button BackBtn;
+    public Button StageChangeBtn;
+
+    public GameObject[] StageList;
+    private int StageCount = 0;
+
+    GameObject player;
     
     // Start is called before the first frame update
     void Start()
     {
+        //프레임 안정화
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+
+        player = GameObject.Find("Player"); //플레이어 찾기 
+
         if (BackBtn != null)
         {
             BackBtn.onClick.AddListener(BackBtnClick);
         }
+
+        if(StageChangeBtn != null)
+        {
+            StageChangeBtn.onClick.AddListener(StageChangeBtnClick);
+        }
+
+        
 
     }
 
@@ -22,6 +41,14 @@ public class StageMgr : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    private void StageChangeBtnClick()
+    {
+        StageCount++;
+        player.transform.position = StageList[StageCount].transform.position;
+
     }
 
     private void BackBtnClick()
