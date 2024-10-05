3b7f6d575060f12be86fad1a9b686a15e2c902b4
wodls <wodls1593@gmail.com>
2024-10-05 23:39:34 +0900

    feat: 오디오, 씬관련 추가
    사운드 매니저 생성, 세팅창 생성, AI그림 추가, 오디오믹서추가,

---
+++ Assets/Scenes/2.StageScene/Scripts/MainScreenBG.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScreenBG : MonoBehaviour
+{
+    public GameObject SettingPanel;
+    private bool isSettingPanel = false;
+    public void LoadStageScene()
+    {
+        SceneLoader.LoadStageScene();
+    }
+
+    public void OnSettingPanel()
+    {
+        if (isSettingPanel)
+        {
+            isSettingPanel = false;
+            SettingPanel.SetActive(isSettingPanel);
+        }
+        else
+        {
+            isSettingPanel = true;
+            SettingPanel.SetActive(isSettingPanel);
+        }
+        
+    }
+}
