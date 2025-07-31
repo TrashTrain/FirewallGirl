ade3dfd4c9b85d63be76e2d02f0d9a6a3bb1ca94
tjswodud <tjswodud85@gmail.com>
2025-07-31 22:57:11 +0900

    fix: modified card deck spreading logic fix: modified player's cost ui

--- Assets/Scenes/4.BattleScene/Scripts/UI/CardDeckController.cs
+++ Assets/Scenes/4.BattleScene/Scripts/UI/CardDeckController.cs
@@ -16,6 +16,8 @@ public class CardDeckController : MonoBehaviour, IPointerEnterHandler, IPointerE
 
     public bool isSpread = false;
     
+    float snapThreshold = 0.01f;
+    
     void Start()
     {
         // 자식 UI 카드 RectTransform 모으기
