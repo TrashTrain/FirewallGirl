9ccccc6abdd1906bc12aadf4a54fc146e292fe7f
JaeYoung Seon <42963882+tjswodud@users.noreply.github.com>
2026-04-12 15:49:09 +0900

    fix: 몬스터 UI가 카드 UI보다 위에 있는 문제 해결

--- Assets/Scenes/4.BattleScene/Scripts/UI/PanelToggler.cs
+++ Assets/Scenes/4.BattleScene/Scripts/UI/PanelToggler.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class PanelToggler : MonoBehaviour
 {
     public GameObject targetPanel;
+    
+    private void Update()
+    {
+        // ESC 키가 눌렸을 때 (한 번 누를 때마다 1회 실행)
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePanel();
+        }
+    }
 
     // 버튼의 OnClick 이벤트에 연결할 함수
     public void TogglePanel()
